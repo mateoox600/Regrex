ccecf1d637d035cabcf36e90f1c417eb7f6de242
mateoox600 <matboi862@gmail.com>
2022-08-18 23:40:37 +0200

    Pattern and flags are not private

--- src/regrex.py
+++ src/regrex.py
@@ -4,17 +4,25 @@ from re import compile, Pattern
 class Regrex:
 
     def __init__(self, pattern: Pattern, flags: int):
-        self.pattern = pattern
-        self.flags = flags
+        self._pattern = pattern
+        self._flags = flags
         self.compile_cache = None
 
     def compile(self, force=False):
         if force or self.compile_cache is None:
-            self.compile_cache = compile(self.pattern.pattern, self.flags)
+            self.compile_cache = compile(self._pattern.pattern, self._flags)
         return self.compile_cache
 
     def match(self, string: str):
         return self.compile().match(string)
+
+    def set_pattern(self, new_pattern: Pattern):
+        self._pattern = new_pattern
+        self.compile_cache = None
+
+    def set_flags(self, new_flags: int):
+        self._flags = new_flags
+        self.compile_cache = None
 
 def regrex(f: Callable[..., tuple[Pattern, int]]):
     pattern, flag = f()
